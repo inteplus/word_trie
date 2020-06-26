e3bf0d6f5712daec2d24b1100144613ed06c4cb1
Minh-Tri Pham <pmtri80@gmail.com>
2020-06-26 11:06:18 +0100

    initial implementation of serialisation

--- cpp/word_trie.cpp
+++ cpp/word_trie.cpp
@@ -6,19 +6,32 @@
 #include "word_trie.h"
 
 
-void Trie_c::insert(std::string const& word, long long count)
-{
+int TrieNode_c::num_descendants() const {
+  int cnt = 1;
+  for(auto iter = m_children.begin(); iter != m_children.end(); iter++)
+    cnt += iter->second->num_descendants();
+  return cnt;
+}
+
+
+int TrieNode_c::update_id_map(std::map<TrieNode_c*, std::pair<int, char> >& node_map, int starting_id, char ch) const {
+  for(auto iter = m_children.begin(); iter != m_children.end(); iter++)
+    starting_id += iter->second->update_id_map(node_map, starting_id, iter->first);
+  node_map[(TrieNode_p)this] = std::make_pair(starting_id, ch);
+  return starting_id+1;
+}
+
+
+void Trie_c::insert(std::string const& word, long long count) {
   TrieNode_p current = root();
-  for(int i=0; i < word.size(); i++)
-  {
+  for(int i=0; i < word.size(); i++) {
     char ch = word[i];
     auto iter = current->m_children.find(ch);
     TrieNode_p node;
 
     if(iter != current->m_children.end())
       node = iter->second.get();
-    else
-    {
+    else {
       node = new TrieNode_c();
       node->m_parent = current;
       current->m_children[ch].reset(node);
@@ -31,11 +44,9 @@ void Trie_c::insert(std::string const& word, long long count)
 }
 
 
-TrieNode_p Trie_c::search(std::string const& word) const
-{
+TrieNode_p Trie_c::search(std::string const& word) const {
   TrieNode_p current = root();
-  for(int i=0; i < word.size(); i++)
-  {
+  for(int i=0; i < word.size(); i++) {
     char ch = word[i];
     auto iter = current->m_children.find(ch);
     TrieNode_p node;
@@ -46,3 +57,60 @@ TrieNode_p Trie_c::search(std::string const& word) const
   }
   return current;
 }
+
+
+TrieSerialisable_c Trie_c::serialisable() const
+{
+  TrieSerialisable_c res;
+  res.m_num_nodes = num_nodes();
+  std::map<TrieNode_p, std::pair<int, char> > id_map;
+  int cnt_id = m_root->update_id_map(id_map, 0, ' ');
+  if(cnt_id != res.m_num_nodes) {
+    std::cerr << "Warning: Number of ids " << cnt_id << " differs from number of nodes " << res.m_num_nodes << "." << std::endl;
+    res.m_num_nodes = cnt_id;
+  }
+
+  res.m_parent.resize(cnt_id);
+  res.m_char.resize(cnt_id);
+  res.m_count.resize(cnt_id);
+  res.m_endofword.resize(cnt_id);
+
+  for(auto iter = id_map.begin(); iter != id_map.end(); iter++) {
+    TrieNode_p node = iter->first;
+    int id = iter->second.first;
+    res.m_parent[id] = id_map[node->m_parent].first;
+    res.m_char[id] = iter->second.second;
+    res.m_count[id] = node->m_count;
+    res.m_endofword[id] = node->m_endofword;
+  }
+
+  return res;
+}
+
+Trie_c TrieSerialisable_c::trie() const
+{
+  Trie_c res;
+  std::vector<TrieNode_p> nodes(m_num_nodes);
+  int i;
+  TrieNode_p node;
+
+  // allocate
+  for(i = 0; i < m_num_nodes; ++i) {
+    node = new TrieNode_c;
+    node->m_count = m_count[i];
+    node->m_endofword = m_endofword[i];
+    nodes[i] = node;
+  }
+
+  // fill in linking attributes
+  for(i = 0; i < m_num_nodes-1; ++i) {
+    node = nodes[i];
+    int parent_id = m_parent[i];
+    node->m_parent = nodes[parent_id];
+    nodes[parent_id]->m_children[m_char[i]].reset(node);
+  }
+
+  res.m_root.reset(nodes[m_num_nodes-1]);
+  return res;
+}
+
--- cpp/word_trie.h
+++ cpp/word_trie.h
@@ -1,6 +1,7 @@
 #include <string>
 #include <memory>
 #include <map>
+#include <vector>
 
 
 struct TrieNode_c;
@@ -8,25 +9,29 @@ typedef TrieNode_c* TrieNode_p;
 typedef std::unique_ptr<TrieNode_c> TrieNode_lp;
 
 
-struct TrieNode_c
-{
+struct TrieNode_c {
   std::map<char, TrieNode_lp> m_children;
   TrieNode_p m_parent;
   long long m_count;
   bool m_endofword;
 
   // constructor
-  TrieNode_c()
-  {
+  TrieNode_c() {
     m_parent = nullptr;
     m_count = 0;
     m_endofword = false;
   }
+
+  // number of descendant nodes, including this node
+  int num_descendants() const;
+
+  // update the map that maps each TrieNode_p to an integer id and a postfix character, returning the last added id plus 1.
+  int update_id_map(std::map<TrieNode_p, std::pair<int, char> >& node_map, int starting_id, char ch) const;
 };
 
 
-struct Trie_c
-{
+struct TrieSerialisable_c;
+struct Trie_c {
   TrieNode_lp m_root;
 
   // constructor
@@ -39,10 +44,28 @@ struct Trie_c
   inline long long total_count() const { return m_root->m_count; }
 
 
+  // count the number of nodes
+  inline int num_nodes() const { return m_root->num_descendants(); }
+
   // inserts a word with count
   void insert(std::string const& word, long long count=1);
 
   // searches for a TrieNode_c representing a word
   TrieNode_p search(std::string const& word) const;
+
+  // turn this structure into an equivalence that is serialisable
+  TrieSerialisable_c serialisable() const;
 };
 
+
+// structure for serializing/deserializing Trie_c. The trie is flattened from leaves to root (root node is the last node).
+struct TrieSerialisable_c {
+  int m_num_nodes;
+  std::vector<int> m_parent;
+  std::vector<char> m_char;
+  std::vector<long long> m_count;
+  std::vector<bool> m_endofword;
+
+  // turn this structure into a trie
+  Trie_c trie() const;
+};
